4c6902ed289271d17b55107c3d133758cc9d28c7
Runar Heggset <r-heggse@online.no>
2017-05-02 20:15:45 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,46 @@
+# lumen-lambda
+ 
+lumen-lambda allows lumen to be executed with php through NodeJS.  
+ 
+## Installation
+
+The project comes with a prebuilt php-cgi-7.1.3. Feel free to build your own.
+
+- npm install
+
+## Usage
+
+- Create a lambda function on aws.
+- Make sure lumen is writing to /tmp or not at all.
+- zip the project with your lumen application placed in the lumen folder. Laravel should also work.
+- Point aws API Gateway to your newly generated lambda function.
+
+For more indepth tips, read this excellent blog post: https://cwhite.me/hosting-a-laravel-application-on-aws-lambda/.
+
+## Contributing
+ 
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+ 
+## History
+ 
+Version 1.0 (2017-05-02) - Initial features
+ 
+## Credits
+ 
+Chris White (https://cwhite.me/)
+ 
+## License
+ 
+The MIT License (MIT)
+
+Copyright (c) 2017 Runar Heggset
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,52 @@
+var spawn = require('child_process').spawn;
+var parser = require('http-string-parser');
+
+exports.handler = function(event, context) {
+    // Sets some sane defaults here so that this function doesn't fail when it's not handling a HTTP request from
+    // API Gateway.
+    var requestMethod = event.httpMethod || 'GET';
+    var serverName = event.headers ? event.headers.Host : '';
+    var requestUri = event.path || '';
+    var headers = {};
+
+    // Convert all headers passed by API Gateway into the correct format for PHP CGI. This means converting a header
+    // such as "X-Test" into "HTTP_X-TEST".
+    if (event.headers) {
+        Object.keys(event.headers).map(function (key) {
+            headers['HTTP_' + key.toUpperCase()] = event.headers[key];
+        });
+    }
+
+    // Spawn the PHP CGI process with a bunch of environment variables that describe the request.
+    var php = spawn('./php-cgi', ['lumen/public/index.php'], {
+        env: Object.assign({
+            REDIRECT_STATUS: 200,
+            REQUEST_METHOD: requestMethod,
+            SCRIPT_FILENAME: 'lumen/public/index.php',
+            SCRIPT_NAME: '/index.php',
+            PATH_INFO: '/',
+            SERVER_NAME: serverName,
+            SERVER_PROTOCOL: 'HTTP/1.1',
+            REQUEST_URI: requestUri
+        }, headers)
+    });
+
+    // Listen for output on stdout, this is the HTTP response.
+    var response = '';
+    php.stdout.on('data', function(data) {
+        response += data.toString('utf-8');
+    });
+
+    // When the process exists, we should have a complete HTTP response to send back to API Gateway.
+    php.on('close', function(code) {
+        // Parses a raw HTTP response into an object that we can manipulate into the required format.
+        var parsedResponse = parser.parseResponse(response);
+
+        // Signals the end of the Lambda function, and passes the provided object back to API Gateway.
+        context.succeed({
+            statusCode: parsedResponse.statusCode || 200,
+            headers: parsedResponse.headers,
+            body: parsedResponse.body
+        });
+    });
+};
